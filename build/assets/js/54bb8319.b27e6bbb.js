46b61406a1735f1edb3f222185436265cc387f79
Vitalii Hromyk <hromykk@gmail.com>
2025-08-07 09:45:16 +0300

    add static

---
+++ build/assets/js/54bb8319.b27e6bbb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[241],{7708:a=>{a.exports=JSON.parse('{"tag":{"label":"AK","permalink":"/tags/ak","allTagsPath":"/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/tags/ak","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ build/assets/js/7798882b.53b83217.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[98],{6063:a=>{a.exports=JSON.parse('{"tag":{"label":"CO","permalink":"/tags/co","allTagsPath":"/tags","count":1,"unlisted":false},"listMetadata":{"permalink":"/tags/co","page":1,"postsPerPage":10,"totalPages":1,"totalCount":1,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
